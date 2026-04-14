0bdec8c2871ecae66a960ede50fc0d34b66633ae
sharan-maker <saisharan255@email.com>
2026-04-14 16:01:45 +0530

    Fix: correct case sensitive path for user model

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 // REGISTER - Create new user account
 exports.register = async (req, res) => {
